98cff1e1616a46d858ed847c86060621107bc51c
Felip Ladron <fladron@omitsis.com>
2015-03-11 09:54:28 +0100

    Removed wysiwyg ckeditor settings
    It may be done in the ckeditor library itself

--- sites/all/modules/omitsis/omitsis_SITEID/js/omitsis_SITEID_ckeditor_config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Custom config for Ckeditor
- */
-
-CKEDITOR.editorConfig = function( config ) {
-  // Activate automatic AFC mode (if not already set).
-  config.allowedContent = true;
-  config.extraPlugins = "iframe";
-};
